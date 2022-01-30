47646151f247b369e4ce0c4f7a8884a4fd22e8c0
cloudguruab <ap.brown011@gmail.com>
2022-01-30 16:58:33 -0500

    updated schema, needs mutations for creating accounts

--- api/schema.py
+++ api/schema.py
@@ -1,9 +1,17 @@
+import graphene
+from .dev.mixins import NodeMixin
 from graphene import Schema
+from .config import Config
 
 class Query(graphene.ObjectType):
-    pass
+    get_balance = graphene.String(at=graphene.String(default_value=Config.DEMO_ACCT))
+    
+    def resolve_get_balance(root, info, at):
+        trigger = NodeMixin()
+        balance = trigger.get_balance(at)
+        return f'Your current account balance is: {balance}'
 
-class Mutation(graphene.Mutation):
-    pass
+# class Mutation(graphene.Mutation):
+#     pass
 
-schema = None
+schema = Schema(query=Query)
